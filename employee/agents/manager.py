f33cd7609fd39a2b77157a828fd63f42a7110d52
Devansh Rathor <devanshrathor123@gmail.com>
2024-01-14 06:59:47 +0530

    Update delivery agent data and display orders on dashboard

--- employee/agents/manager.py
+++ employee/agents/manager.py
@@ -1,4 +1,15 @@
-from models.models import Status, Statuses, StatusQuery, GetStatus
+from models.models import (
+    Status,
+    Statuses,
+    StatusQuery,
+    GetStatus,
+    CreateOrder,
+    Success,
+    Order,
+    OrderID,
+    GetOrders,
+    Orders,
+)
 from uagents import Agent, Context
 
 from protocols.delivery_protocol import delivery_protocol
@@ -13,10 +24,24 @@ manager = Agent(
 print("Manager: ", manager.address)
 
 
+@manager.on_event("startup")
+async def startup_handler(ctx: Context):
+    if not ctx.storage.has("orders"):
+        ctx.storage.set("orders", [])
+
+    if not ctx.storage.has("statuses"):
+        ctx.storage.set("statuses", [])
+
+    if not ctx.storage.has("order_id"):
+        ctx.storage.set("order_id", 0)
+
+    ctx.logger.info("Manager started")
+
+
 @manager.on_interval(period=10)
 async def update_status(ctx: Context):
     print("-" * 20)
-    ctx.logger.info("Status requested", delivery_protocol.digest)
+    ctx.logger.info(f"Status requested: {delivery_protocol.digest}")
     await ctx.experimental_broadcast(
         destination_protocol=delivery_protocol.digest, message=StatusQuery()
     )
@@ -24,15 +49,77 @@ async def update_status(ctx: Context):
 
 @manager.on_message(model=Status)
 async def status_handler(ctx: Context, sender: str, status: Status):
-    ctx.logger.info("Status received", sender, status)
-    ctx.storage.set(sender, status.dict())
+    ctx.logger.info(f"Status received: {status.json()}")
+
+    statuses = ctx.storage.get("statuses")
+    statuses = list(filter(lambda s: s["address"] != sender, statuses))
+    statuses.append(status.dict())
+    ctx.storage.set("statuses", statuses)
 
 
 @manager.on_query(model=GetStatus, replies=Statuses)
 async def get_status_handler(ctx: Context, sender: str, _: GetStatus):
-    statuses = []
-    for address in ctx.storage._data.keys():
-        status = ctx.storage.get(address)
-        statuses.append(Status(**status))
-
+    statuses = [Status(**status) for status in ctx.storage.get("statuses")]
     await ctx.send(sender, Statuses(statuses=statuses))
+
+
+@manager.on_query(model=CreateOrder, replies=Success)
+async def create_order_handler(ctx: Context, sender: str, orderQuery: CreateOrder):
+    next_id = ctx.storage.get("order_id")
+
+    order = {**orderQuery.dict(), "status": "PENDING", "id": next_id}
+    ctx.storage.set("order_id", next_id + 1)
+
+    orders = ctx.storage.get("orders")
+    orders.append(order)
+    ctx.storage.set("orders", orders)
+
+    await ctx.send(sender, Success(success=True))
+
+
+@manager.on_interval(period=10)
+async def assign_order(ctx: Context):
+    orders = ctx.storage.get("orders")
+    statuses = ctx.storage.get("statuses")
+
+    for status in statuses:
+        if status["status"] == "IDLE":
+            if len(orders) > 0:
+                order = filter(lambda order: order["status"] == "PENDING", orders)[0]
+
+                for o in orders:
+                    if o["id"] == order["id"]:
+                        o["status"] = "ASSIGNED"
+                        o["delivery_agent"] = status["name"]
+                        break
+
+                ctx.storage.set("orders", orders)
+
+                await ctx.send(
+                    status["address"], OrderID(id=order["id"], status="ASSIGNED")
+                )
+                break
+
+
+@manager.on_message(model=OrderID)
+async def order_id_handler(ctx: Context, sender: str, order_id: OrderID):
+    orders = ctx.storage.get("orders")
+
+    for order in orders:
+        if order["id"] == order_id.id:
+            order["status"] = order_id.status
+            ctx.storage.set("orders", orders)
+            break
+
+
+@manager.on_query(model=GetOrders, replies=Orders)
+async def get_orders_handler(ctx: Context, sender: str, _: GetOrders):
+    orders = [Order(**order) for order in ctx.storage.get("orders")]
+    await ctx.send(sender, Orders(orders=orders))
+
+
+@manager.on_event("shutdown")
+async def shutdown_handler(ctx: Context):
+    ctx.logger.info("Shutting down")
+    ctx.storage.set("statuses", [])
+    await ctx.stop()
--- employee/app.py
+++ employee/app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request, render_template
-from uagents.query import query
-from flask_cors import CORS
 import json
 
 from agents.manager import manager
-
-
-from models.models import GetStatus
+from flask import Flask, render_template, request
+from flask_cors import CORS
+from models.models import GetStatus, GetOrders
+from uagents.query import query
 
 app = Flask(__name__)
 CORS(app)
@@ -15,19 +13,16 @@ destination = manager.address
 
 
 @app.get("/")
-def index():
-    return render_template("index.html", message="")
-
-
-@app.post("/")
-async def form():
-    data = request.form["data"]
+async def index():
     res = await query(destination=destination, message=GetStatus())
-    print(res.decode_payload())
-    statuses = json.loads(res.decode_payload())["statuses"]
-    print(statuses)
+    statuses = json.loads(res.decode_payload())["statuses"] if res else []
 
-    return render_template("index.html", statuses=statuses)
+    res = await query(destination=destination, message=GetOrders())
+    orders = json.loads(res.decode_payload())["orders"] if res else []
+
+    print(statuses, orders)
+
+    return render_template("index.html", statuses=statuses, orders=orders)
 
 
 if __name__ == "__main__":
